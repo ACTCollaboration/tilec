b0e7d390fa1ff77c82b58c40adc0b4944d8ebff9
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-12-03 02:04:15 -0500

    sehgal

--- bin/sehgal.py
+++ bin/sehgal.py
@@ -4,19 +4,25 @@ from pixell import enmap,reproject
 import numpy as np
 import os,sys
 import healpy as hp
+from orphics.maps import distribute, populate
 
 croot = "/home/msyriac/data/sims/sehgal/colin/"
 
-
-hfile = croot + "healpix_4096_KappaeffLSStoCMBfullsky.fits" #"143_skymap_healpix_Nside4096_DeltaT_uK_SimLensCMB_tSZrescale0p75_CIBrescale0p75_Comm_synchffAME_rad_pts_fluxcut148_7mJy_lininterp.fits"
-
+        
 res = 0.5
 shape,wcs = enmap.fullsky_geometry(res=np.deg2rad(res/60.))
 
-hmap = hp.read_map(hfile)
-omap = reproject.enmap_from_healpix(hmap, shape, wcs, rot=None)
-
+hfile = croot + "healpix_4096_KappaeffLSStoCMBfullsky.fits" #"143_skymap_healpix_Nside4096_DeltaT_uK_SimLensCMB_tSZrescale0p75_CIBrescale0p75_Comm_synchffAME_rad_pts_fluxcut148_7mJy_lininterp.fits"
 
 box = np.deg2rad([[-1,-1],[10,10]])
-cmap = omap.submap(box)
+#shape,wcs = enmap.fullsky_geometry(res=np.deg2rad(res/60.))
+# shape,wcs = enmap.geometry(pos=box,res=np.deg2rad(res/60.))
+
+hmap = hp.read_map(hfile)
+ofunc = lambda shape,wcs : reproject.enmap_from_healpix_interp(hmap, shape, wcs, rot=None,interpolate=True)
+
+cmap = populate(shape,wcs,ofunc)
+
+io.plot_img(cmap,"fmap.png")
+cmap = cmap.submap(box)
 io.hplot(cmap,"cmap")
