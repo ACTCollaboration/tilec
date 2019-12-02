a4184fcc70c624ba2f6412956f0cdb96dfdd6c8f
Mathew Madhavacheril <mathewsyriac@gmail.com>
2019-12-02 14:20:12 -0500

    compare with previous version

---
+++ bin/compare_versions.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.fontset"] = "cm"
+from orphics import maps,io,cosmology,catalogs,stats
+from pixell import enmap,reproject
+import numpy as np
+import os,sys
+from soapack import interfaces as sints
+import tilec.fg as tfg
+import tilec.utils as tutils
+
+depdict = {'cmb':[None,'tsz','cib'],'tsz':[None,'cmb','cib']}
+tdirs = ['/scratch/r/rbond/msyriac/data/depot/tilec/','/scratch/r/rbond/msyriac/data/depot/tilec/v1.0.0_rc_20190919/']
+versions = ['v1.1.0','v1.0.0_rc']
+bin_edges = np.arange(20,6000,20)
+
+for region in ['deep56','boss']:
+
+    mask = sints.get_act_mr3_crosslinked_mask(region)
+    modlmap = mask.modlmap()
+    lmap = mask.lmap()
+    binner = stats.bin2D(modlmap,bin_edges)
+    def pow(x,y=None):
+        k = enmap.fft(x,normalize='phys')
+        ky = enmap.fft(y,normalize='phys') if y is not None else k
+        p = (k*ky.conj()).real
+        cents,p1d = binner.bin(p)
+        return p,cents,p1d
+
+    for component in ['cmb','tsz']:
+        deprojs = depdict[component]
+        for deproj in deprojs:
+            ps = []
+            for tdir,version in zip(tdirs,versions):
+                csfile = tutils.get_generic_fname(tdir,region,component,deproject=deproj,data_comb='joint',version=version)
+                imap = enmap.read_map(csfile)
+                _,cents,p1d = pow(imap)
+                ps.append(p1d.copy())
+            p1 = ps[0]
+            p2 = ps[1]
+            r = (p1-p2)/p2
+
+            pl = io.Plotter('Dell')
+            pl.add(cents,p1,label=versions[0],lw=1)
+            pl.add(cents,p2,label=versions[1],lw=1)
+            pl.done(f"vcomp_{region}_{component}_{str(deproj)}.png")
+
+            pl = io.Plotter('rCell')
+            pl.add(cents,r,lw=1)
+            pl.hline(y=0)
+            pl._ax.set_ylim(-0.2,0.2)
+            pl.done(f"vcompdiff_{region}_{component}_{str(deproj)}.png")
+            
